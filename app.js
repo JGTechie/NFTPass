3fac83bbce9f21b930ec582de3cbb80ab4b0a508
Yichuan Guo <yichuanguo@macbook-air-5.home>
2023-11-19 19:09:49 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Sample London Tube station names
+const tubeStations = [
+  'Paddington',
+  'Victoria',
+  'King\'s Cross St Pancras',
+  'Liverpool Street',
+  'Waterloo',
+];
+
+// Route to get all station names
+app.get('/stations', (req, res) => {
+  res.json({ stations: tubeStations });
+});
+
+// Route to get information about a specific station
+app.get('/stations/:stationName', (req, res) => {
+  const stationName = req.params.stationName;
+  const stationInfo = {
+    name: stationName,
+    // You can add more information about the station if needed
+  };
+  res.json(stationInfo);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
